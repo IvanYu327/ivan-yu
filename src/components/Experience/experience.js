51ef3a1ca8b7c4a12123b4aa72368b371bd7c7ec
Ivan <ivanyu327@gmail.com>
2022-03-26 03:55:32 -0400

    funny monke moment

---
+++ src/components/Experience/experience.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div>
+      pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg
+      pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg
+      pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg
+      pg pg pg pg pg pg pg pg pg pg pg pg pg pg pg
+    </div>
+  );
+};
+
+export default Experience;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,18 @@
 import React from "react";
+
+import { ContentWrapper, ContentPage } from "../components/contentWrapper";
+
 import About from "../components/About/about";
+import Experience from "../components/Experience/experience";
 
 const Home = () => {
   return (
-    <>
-      <About />
-    </>
+    <ContentPage>
+      <ContentWrapper>
+        <About />
+        <Experience />
+      </ContentWrapper>
+    </ContentPage>
   );
 };
 
