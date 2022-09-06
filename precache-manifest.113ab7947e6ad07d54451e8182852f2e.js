7022d7cd9cb5ea8b6898ae059269c4f0e5663ad9
Ivan <ivanyu327@gmail.com>
2022-09-06 19:36:29 -0400

    Updates

---
+++ precache-manifest.113ab7947e6ad07d54451e8182852f2e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "af6e1207249d2ef6f250",
+    "url": "/personal-website/static/css/main.c88cdb91.chunk.css"
+  },
+  {
+    "revision": "af6e1207249d2ef6f250",
+    "url": "/personal-website/static/js/main.a75e4b76.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "61add358db92b5d93e7e",
+    "url": "/personal-website/static/js/2.e673fd04.chunk.js"
+  },
+  {
+    "revision": "eefa72967238ca25e2ee2e84dcf8534c",
+    "url": "/personal-website/static/media/cognixion.eefa7296.png"
+  },
+  {
+    "revision": "149113b747631dfa3ede1f9b15ab5194",
+    "url": "/personal-website/static/media/TimeLine2.149113b7.png"
+  },
+  {
+    "revision": "e4cd9a87c3199d49eb48247cd8270669",
+    "url": "/personal-website/static/media/Tabis.e4cd9a87.png"
+  },
+  {
+    "revision": "3e8560ab2b6c9ac813b9a7549e00184c",
+    "url": "/personal-website/static/media/StudyGoose.3e8560ab.png"
+  },
+  {
+    "revision": "7223a4fbfbe5383eacc193a0969a5e64",
+    "url": "/personal-website/static/media/FreeSpACE.7223a4fb.png"
+  },
+  {
+    "revision": "bd4402d7dbc42b9061a7642a49e3f7a3",
+    "url": "/personal-website/static/media/CXNone.bd4402d7.png"
+  },
+  {
+    "revision": "920c71d3c24d47cfa9a0de6eb034024a",
+    "url": "/personal-website/static/media/Proxima-Nova-Regular.920c71d3.woff2"
+  },
+  {
+    "revision": "1d7f11289378359bbc815500a5f9f2b5",
+    "url": "/personal-website/static/media/BespokeSerif-Bold.1d7f1128.otf"
+  },
+  {
+    "revision": "bbddd815c56104cee0e6423e0bbe60c0",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3684acdcf5f9fb7dc05643d1e6e2d16b.js"
+  "/personal-website/precache-manifest.113ab7947e6ad07d54451e8182852f2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/personal-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
