662d081eb4fd2a51adcbe45d3f4cf58e834d6592
Ivan <ivanyu327@gmail.com>
2022-09-19 04:05:01 -0400

    fixed mobile formatting on experience

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -20,33 +20,39 @@ const Experience = () => {
   return (
     <Section>
       <Container flex={true}>
-        <Grid container spacing={1} columns={{ xs: 4, sm: 8, md: 12 }}>
-          <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-            <WorkContainer>
-              <Heading2>Where I'm Working.</Heading2>
-              <Line marginTop={0} />
-              {CURRENT_EXPERIENCE.map((experience) => (
-                <ExperienceCard
-                  key={CURRENT_EXPERIENCE.indexOf(experience)}
-                  experience={experience}
-                />
-              ))}
-            </WorkContainer>
-          </Grid>
+        {/* <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}> */}
+        <WorkContainer>
+          <Heading2>Where I'm Working.</Heading2>
+          <Line marginTop={0} />
+          {CURRENT_EXPERIENCE.map((experience) => (
+            <ExperienceCard
+              key={CURRENT_EXPERIENCE.indexOf(experience)}
+              experience={experience}
+            />
+          ))}
+        </WorkContainer>
+        {/* </Grid>
 
-          <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-            <WorkContainer>
-              <Heading2>Where I've Worked.</Heading2>
-              <Line marginTop={0} />
-              {PAST_EXPERIENCE.map((experience) => (
-                <ExperienceCard
-                  key={PAST_EXPERIENCE.indexOf(experience)}
-                  experience={experience}
-                />
-              ))}
-            </WorkContainer>
-          </Grid>
-        </Grid>
+          <Grid item xs={6} sm={6} md={6} lg={6} xl={6}> */}
+        <WorkContainer>
+          <Heading2>Where I've Worked.</Heading2>
+          <Line marginTop={0} />
+          {PAST_EXPERIENCE.map((experience) => (
+            <ExperienceCard
+              key={PAST_EXPERIENCE.indexOf(experience)}
+              experience={experience}
+            />
+          ))}
+        </WorkContainer>
+        {/* </Grid> */}
+        {/* </Grid> */}
       </Container>
     </Section>
   );
